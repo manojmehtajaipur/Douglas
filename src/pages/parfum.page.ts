6d7c5c291e7fa43190ad24e968b28a0f54fedf16
manojmehtajaipur <mkmehta51@gmail.com>
2025-03-27 22:33:18 +0530

    Update logs

--- src/pages/parfum.page.ts
+++ src/pages/parfum.page.ts
@@ -4,9 +4,12 @@ import { HelperClass } from '../../helper/helper';
 export class ParfumPageClass {
    readonly page: Page
    helperpage : HelperClass
-   constructor(page: Page) {
+   testlogs 
+
+   constructor(page: Page, testlogs) {
       this.page = page
       this.helperpage = new HelperClass(this.page);
+      this.testlogs = testlogs
    }
 
    public perfumTab = '[aria-label="PARFUM"]'
@@ -18,5 +21,6 @@ export class ParfumPageClass {
       await this.helperpage.wait(this.aktionenFilter)
       await this.helperpage.click(this.aktionenFilter)
       await this.helperpage.click(this.filter(filter))
+      await this.testlogs.write(`Select filter : ${filter} \n`);
    }
 }
--- src/specs/test.spec.ts
+++ src/specs/test.spec.ts
@@ -5,54 +5,57 @@ import { HelperClass } from '../../helper/helper';
 import data from '../../fixtures/aktionenfilters.json'
 import config from '../../fixtures/userconfig.json'
 import classname from '../../fixtures/classname.json'
+import file from 'fs'
 
-let homepage, parfumpage, helperpage;
+let homepage, parfumpage, helperpage, logStream;
 
 test.beforeEach(async ({ page }) => {
     homepage = new HomePageClass(page);
-    parfumpage = new ParfumPageClass(page);
+    parfumpage = new ParfumPageClass(page, logStream);
     helperpage = new HelperClass(page);
-    page.on('console', msg => {console.log(msg)});
+    logStream = file.createWriteStream(`documents/logs${new Date().toJSON().slice(0,10)}.txt`, { flags: 'a' });
 });
 
 test.describe('Douglas Home page Test', () => {
 
     test('Verify aktionen filters are not on Parfum page without selecting', async ({ page }) => {
-
         await helperpage.navigate(config.url);
+        await logStream.write(`Navigate to : ${config.url} \n`);
 
         await homepage.acceptCookiesConsent();
 
         await homepage.clickOnParfumTab();
         await expect(page.locator(homepage.perfumTab)).toHaveClass(classname.selectedTab);
+        await logStream.write(`User is on Parful page\n`);
 
         await expect(page.locator(parfumpage.filterContainer)).not.toHaveText([data.filter3, data.filter2, data.filter1])
     });
 
     test('Verify aktionen filters on Parfum page', async ({ page }) => {
-
         await helperpage.navigate(config.url);
+        await logStream.write(`Navigate to : ${config.url} \n`);
 
         await homepage.acceptCookiesConsent();
 
         await homepage.clickOnParfumTab();
         await expect(page.locator(homepage.perfumTab)).toHaveClass(classname.selectedTab);
 
-        console.log('Apply filters' + data.filter1 + data.filter1 + data.filter1)
         await parfumpage.selectAktionenFilter(data.filter1)
         await parfumpage.selectAktionenFilter(data.filter2)
         await parfumpage.selectAktionenFilter(data.filter3)
 
         await expect(page.locator(parfumpage.filterContainer)).toHaveText([data.filter3, data.filter2, data.filter1])
+        await logStream.write(`Verify filters on Parfum page : ${data.filter1}, ${data.filter1}, ${data.filter1}`)
+
     });
 
     test('Fail Test: Verify retry and screenshot capture on fail scenario', async ({ page }) => {
-
         await helperpage.navigate(config.url);
+        await logStream.write(`Navigate to : ${config.url} \n`);
 
         await homepage.acceptCookiesConsent();
 
-        await console.error('Verifying Parfum page without click on Parfum tab')
+        await logStream.write(`Verifying Parfum page without click on Parfum tab \n`)
         await expect(page.locator(homepage.perfumTab)).toHaveClass(classname.selectedTab);
     });
 
